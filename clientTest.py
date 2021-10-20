5ef9be0a810a397aadf092889ab5371ceef14129
Leeksun <choleeksun@gmail.com>
2021-10-20 04:47:11 +0100

    I hope this works

--- clientTest.py
+++ clientTest.py
@@ -1,7 +1,10 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+host_ip = socket.gethostbyname(host)
+port = 8080
 client.connect(('192.168.1.86', 8080))
-client.send("I am CLIENT\n".encode())
+client.send("I am CLIENT".encode())
 from_server = client.recv(4096)
 client.close()
-print(from_server)
+print(from_server.decode('ascii'))
